1c7b93599ef8b6086a466f59a647b424fa7b929d
MDevoldere <mdevoldere@users.noreply.github.com>
2023-04-26 12:39:17 +0200

    TryParse

---
+++ 01 - Algo/01-introduction/Demo_01_Variables/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Demo_01_Variables
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Variables et typage statique");
+
+            byte AAA; // entier 8 bits 
+            short AA; // entier 16 bits
+            ushort AA2;
+           // int A; // Entier 32bits (-2147000000 --> 2147000000)
+            uint A2; // entier 32bits non signé (0 à 4.294000000)
+            long B; // Entier 64bits 
+            ulong B2;
+            float C;
+            double D;
+
+
+
+            int A;
+            bool saisieOk = false;
+            int result;
+
+            string saisie = "23";
+
+            do
+            {
+                Console.WriteLine("Saisissez un nombre");
+                saisie = Console.ReadLine();
+                
+            }
+            while (!int.TryParse(saisie, out result));
+
+          /*  do
+            {
+                Console.WriteLine("Saisissez un nombre");
+                saisie = Console.ReadLine();
+                saisieOk = int.TryParse(saisie, out result);
+            }
+            while (!saisieOk);*/
+
+            Console.WriteLine(result);
+
+            Console.Read();
+
+        }
+    }
+} 
