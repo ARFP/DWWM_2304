702ce0c0707a652d8ca602aac63827b5dd9de8f5
MDevoldere <mdevoldere@users.noreply.github.com>
2023-04-28 11:15:54 +0200

    retraite  algo

---
+++ 01 - Algo/01-introduction/Exo_2_1_Tri_Nombres/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Exo_2_1_Tri_Nombres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+
+            Console.WriteLine("Saisissez un premier nombre entier : ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Saisissez un second nombre entier : ");
+            b = int.Parse(Console.ReadLine());
+
+            string resultat;
+
+            if (b < a)
+            {
+                resultat = b + " - " + a;
+                Console.WriteLine(b + " - "+ a);
+            }
+            else
+            {
+                resultat = a + " - " + b;
+                Console.WriteLine(a + " - " + b);
+            }
+
+           
+            
+            
+            resultat = (b < a ? b + "-" + a : a + "-" + b);
+
+
+            bool res = a < b ? true : false;
+
+            if(a < b)
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+
+            Console.WriteLine("Avez vous faim ? (Y/n)");
+
+            res = a < b;
+
+            res = Console.ReadLine() == "oui";
+
+            res = bool.Parse(Console.ReadLine());
+
+        }
+    }
+}
---
+++ 01 - Algo/01-introduction/Exo_2_3_Retraite/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exo_2_3_Retraite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
