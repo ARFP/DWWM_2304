0d7de908c8e4b483009b1309fd0c91703c59dda5
MDevoldere203 <mdevoldere@users.noreply.github.com>
2024-03-07 12:48:35 +0100

    Create ApiPays ASP

---
+++ 06 - ASP/MyFirstApi/ApiPays/Database/CountriesDbContext.cs
@@ -0,0 +1,23 @@
+﻿using ApiPays.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPays.Database
+{
+    public class CountriesDbContext : DbContext
+    {
+        IConfiguration appConfig;
+
+        public DbSet<Country> Countries { get; set; }
+
+        public CountriesDbContext(IConfiguration config)
+        {
+            appConfig = config;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(appConfig.GetConnectionString("Default"));
+        }
+    }
+}
